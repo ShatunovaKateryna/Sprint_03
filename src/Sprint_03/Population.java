e24a937a697c6e3947c9e28ae2ceba5619190d0a
ShatunovaKateryna <kateshatunova@gmail.com>
2023-10-19 10:01:44 +0300

    changed Population

--- src/Sprint_03/Population.java
+++ src/Sprint_03/Population.java
@@ -1,5 +1,4 @@
 package Sprint_03;
-
 public class Population {
     //Shatunova Kateryna CS-12
     public static void main(String[] args) {
@@ -11,7 +10,7 @@ public class Population {
     private static final int deathsPerYear=secondsInYear/7;
     private static final int departuresPerYear=secondsInYear/45;
     public static void Display(){
-        for (int i=2024; i<=2028; i++){
+        for (int i=2023; i<=2028; i++){
             System.out.println("Total population for "+i+" is "+(totalPopulationIn2023+=birthsPerYear-deathsPerYear-departuresPerYear));
         }
     }
