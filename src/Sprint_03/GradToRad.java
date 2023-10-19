253ad7216be3f1a75ddd944ffbac7379f898ba5b
ShatunovaKateryna <kateshatunova@gmail.com>
2023-10-19 09:53:09 +0300

    add formula

--- src/Sprint_03/GradToRad.java
+++ src/Sprint_03/GradToRad.java
@@ -1,14 +1,14 @@
 package Sprint_03;
-
 import java.util.Scanner;
-
 public class GradToRad {
     //Shatunova Kateryna CS-12
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter degrees");
         double degrees=scanner.nextDouble();
-        double radians=Math.toRadians(degrees);
+        double PI=3.14159265;
+        //Math.toRadians(degrees);
+        double radians=degrees*PI/180;
         System.out.println("Degrees to radians is "+radians);
     }
 }
--- src/Sprint_03/RadToGrad.java
+++ src/Sprint_03/RadToGrad.java
@@ -1,14 +1,14 @@
 package Sprint_03;
-
 import java.util.Scanner;
-
 public class RadToGrad {
     //Shatunova Kateryna CS-12
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter radians");
+        System.out.println("Enter radians (like this 1,57)");
         double radians=scanner.nextDouble();
-        double degrees=Math.toDegrees(radians);
+        double Pi=3.14159265;
+        //Math.toDegrees(radians);
+        double degrees=radians*180/Pi;
         System.out.println("Radians to degrees is "+degrees);
     }
 }
