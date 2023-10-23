4ec4e1a7ddb2d618d35a376f94491a41733fdd32
ShatunovaKateryna <kateshatunova@gmail.com>
2023-10-23 16:58:21 +0300

    changed Speed

--- src/Sprint_03/Speed.java
+++ src/Sprint_03/Speed.java
@@ -1,20 +1,17 @@
 package Sprint_03;
-
 public class Speed {
     //Shatunova Kateryna CS-12
     public static void main(String[] args) {
         //data entry
-        double minutes=45.0/60.0;
-        double seconds=0.30/60;
-        double time=minutes+seconds;
-
+        double minutes=45.0;
+        double seconds=0.30;
         int distance=14;
-        double DistanceInMiles=14/1.6;
         double AverageSpeed;
-        //calculation
+        //Calculation
+        double time=(minutes+seconds)/60;
+        double DistanceInMiles=distance/1.6;
         AverageSpeed=DistanceInMiles/time;
-
-        //Display !!!
+        //Output
         System.out.println("Average speed in miles is "+AverageSpeed);
     }
 }
