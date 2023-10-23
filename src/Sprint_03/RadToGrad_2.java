2ae6dfbb72091530a5527f5b1b5efd758219d396
ShatunovaKateryna <kateshatunova@gmail.com>
2023-10-23 19:21:07 +0300

    second version RadToGrad_2

---
+++ src/Sprint_03/RadToGrad_2.java
@@ -0,0 +1,14 @@
+package Sprint_03;
+
+public class RadToGrad_2 {
+    //Shatunova Kateryna CS-12
+    public static void main(String[] args) {
+        //data entry
+        double radians=1.5707963249999999;
+        double PI=3.14159265;
+        //calculation
+        double degrees=radians*180/PI;
+        //output
+        System.out.println(radians+" radians is "+degrees+" degrees");
+    }
+}
