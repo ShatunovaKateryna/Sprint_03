7fb466f9a1b49f553b46e760f4368658ea389c7a
ShatunovaKateryna <kateshatunova@gmail.com>
2023-10-23 16:56:49 +0300

    changed Circle2

--- src/Sprint_03/Сircle.java
+++ src/Sprint_03/Сircle.java
@@ -3,9 +3,9 @@ public class Сircle {
     //Shatunova Kateryna CS-12
     public static void main(String[] args) {
         //data entry
-        double radius, area, perimeter;
+        double radius=5.5;
+        double area, perimeter;
         double PI=3.14159265;
-        radius=5.5;
         //calculation
         perimeter=2*radius*PI;
         area=radius*radius*PI;
